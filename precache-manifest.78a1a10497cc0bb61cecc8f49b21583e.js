92d3c8a4835637e9cf4b0d267194e5419c186791
Natalia <NGdigitaldesign@outlook.com>
2020-05-09 08:39:15 +0100

    Updates

---
+++ precache-manifest.78a1a10497cc0bb61cecc8f49b21583e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccaaac8f0233186a727a06a9b9e47c2",
+    "url": "/newone/index.html"
+  },
+  {
+    "revision": "222c88c080dcb3d72d95",
+    "url": "/newone/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "172e9421aa51ec0483f5",
+    "url": "/newone/static/css/main.160bfe88.chunk.css"
+  },
+  {
+    "revision": "222c88c080dcb3d72d95",
+    "url": "/newone/static/js/2.674fff39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newone/static/js/2.674fff39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172e9421aa51ec0483f5",
+    "url": "/newone/static/js/main.80d3b6e4.chunk.js"
+  },
+  {
+    "revision": "713dc43d92ad54229867",
+    "url": "/newone/static/js/runtime-main.fe926960.js"
+  }
+]);
